be0b7163d795fb0d4fe3216ad36494958f78170a
Parveen Sangroya <9700282+sangroya@users.noreply.github.com>
2020-12-18 17:46:35 +0530

    Update ListView.php
    allow to add parent div on the item table

--- ListView.php
+++ ListView.php
@@ -9,12 +9,58 @@
 namespace sangroya;
 use Closure;
 use yii\helpers\ArrayHelper;
+use yii\base\InvalidConfigException;
 use yii\helpers\Html;
 use yii\widgets\ListView as YiiListView;
 
 class ListView extends YiiListView
 {
 
+    public $header=false;
+    public $parentOptions=[];
+   
+    public function renderHeader($attributes){
+        $header="";
+        $options=$this->itemOptions;
+        $tag = ArrayHelper::remove($options, 'tag', 'div');
+        $headerItemTag="span";
+       
+        if($tag=="tr")
+        $headerItemTag="th";
+
+       
+        foreach($attributes as $value)
+        $header.=Html::tag($headerItemTag,$value,[]);
+      
+        return Html::tag($tag,$header,[]);
+    }
+    public function renderItems()
+    {
+        $models = $this->dataProvider->getModels();
+        $keys = $this->dataProvider->getKeys();
+        $rows = [];
+        if($this->header){
+            if(! isset($this->header["attributes"])){
+                throw new InvalidConfigException('The "attributes" property must be either an array or an object of header');
+            }
+            $row[]=$this->renderHeader($this->header['attributes']);
+          
+        }
+        foreach (array_values($models) as $index => $model) {
+            $key = $keys[$index];
+            if (($before = $this->renderBeforeItem($model, $key, $index)) !== null) {
+                $rows[] = $before;
+            }
+
+            $rows[] = $this->renderItem($model, $key, $index);
+
+            if (($after = $this->renderAfterItem($model, $key, $index)) !== null) {
+                $rows[] = $after;
+            }
+        }
+      
+        return implode($this->separator, $rows);
+    }
     /**
      * Runs the widget.
      */
@@ -35,10 +81,17 @@ class ListView extends YiiListView
 
         $options = $this->options;
         $tag = ArrayHelper::remove($options, 'tag', 'div');
-        
+       
         foreach($mycontent as $key=>$content){
             if($key=="{items}")
-            echo Html::tag($tag, $content, $options);
+            {
+                if($this->parentOptions){
+                    $parentOptions = $this->parentOptions;
+                    $parentTag = ArrayHelper::remove($parentOptions, 'tag', 'div');
+                    echo Html::tag($parentTag, Html::tag($tag, $content, $options),$parentOptions);
+                }else
+                echo Html::tag($tag, $content, $options);
+            }
             else
             echo $content;
            
